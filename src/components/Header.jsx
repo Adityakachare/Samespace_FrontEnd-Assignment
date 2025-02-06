d4e6dee5969106ecf43afd580495b7eac917f389
Adityakachare <aadityakachare132@gmail.com>
2025-02-07 03:11:10 +0530

    SongList with dynamic background color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,9 @@ const Header = ({ showTopTracks, setShowTopTracks }) => {
   };
 
   return (
-    <div className="font-spotify flex justify-center sm:justify-start space-x-8 absolute top-12 sm:top-10 left-1/2 sm:left-1/6 transform -translate-x-1/2 sm:translate-x-0">
+    <div className="flex justify-center sm:justify-start space-x-6 absolute top-10 sm:top-6 left-1/2 sm:left-1/4 transform -translate-x-1/2 sm:translate-x-0">
       <h2
-        className={`cursor-pointer text-lg max-[375px]:text-sm sm:text-xl md:text-xl lg:text-2xl font-bold transition-all duration-300 ease-in-out ${
+        className={`cursor-pointer text-base sm:text-lg md:text-xl lg:text-2xl font-bold transition-all duration-300 ease-in-out ${
           !showTopTracks ? "text-white" : "text-white/50"
         }`}
         onClick={() => handleSelection(false)}
@@ -16,7 +16,7 @@ const Header = ({ showTopTracks, setShowTopTracks }) => {
         For You
       </h2>
       <h2
-        className={`cursor-pointer text-lg max-[375px]:text-sm sm:text-xl md:text-xl lg:text-2xl font-bold transition-all duration-300 ease-in-out ${
+        className={`cursor-pointer text-base sm:text-lg md:text-xl lg:text-2xl font-bold transition-all duration-300 ease-in-out ${
           showTopTracks ? "text-white" : "text-white/50"
         }`}
         onClick={() => handleSelection(true)}
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,61 +1,27 @@
-// import React, { useEffect, useState } from "react";
-// import { SiSpotify } from "react-icons/si";
-// import { useSongs } from "../context/SongContext";
-
-// const MainPage = () => {
-//   const { songs } = useSongs();
-//   const [backgroundColor, setBackgroundColor] = useState("#0f0f0f");
-
-//   useEffect(() => {
-//     if (songs && songs.length > 0) {
-//       const selectedSong = songs[0]; // Default to first song
-//       if (selectedSong && selectedSong.accent) {
-//         setBackgroundColor(selectedSong.accent);
-//       }
-//     }
-//   }, [songs]);
-
-//   return (
-//     <div
-//       className="flex flex-col justify-between p-4 min-h-screen transition-colors duration-500"
-//       style={{ backgroundColor }} // Apply dynamic background color
-//     >
-//       <header className="flex items-center space-x-2">
-//         <SiSpotify className="text-white sm:w-6 sm:h-6 md:w-6 md:h-6 lg:w-8 lg:h-8 xl:w-10 xl:h-10" />
-//         <span className="text-white text-lg sm:text-xl md:text-1xl lg:text-2xl xl:text-3xl font-inter font-semibold">
-//           Spotify
-//           <sup className="text-xs sm:text-sm md:text-base lg:text-lg">®</sup>
-//         </span>
-//       </header>
-//     </div>
-//   );
-// };
-
-// export default MainPage;
-
 import React, { useEffect, useState } from "react";
 import { SiSpotify } from "react-icons/si";
 import { useSongs } from "../context/SongContext";
 import Header from "../components/Header"; // Import Header
 import SongList from "../components/SongList"; // Import SongList
-
+import SearchBar from "../components/SearchBar"; // Import SearchBar
 const MainPage = () => {
   const songs = useSongs(); // Fetch songs from context
   const [backgroundColor, setBackgroundColor] = useState("#0f0f0f");
   const [showTopTracks, setShowTopTracks] = useState(false); // State for toggling between For You and Top Tracks
+  const [selectedSong, setSelectedSong] = useState(null);
 
   useEffect(() => {
     if (songs && songs.length > 0) {
-      const selectedSong = songs[0]; // Default to first song
+      // const selectedSong = songs[1]; // Default to first song
       if (selectedSong && selectedSong.accent) {
         setBackgroundColor(selectedSong.accent);
       }
     }
-  }, [songs]);
+  }, [selectedSong]);
 
   return (
     <div
-      className="flex flex-col justify-between p-4 min-h-screen transition-colors duration-500"
+      className="flex flex-col justify-start p-4 min-h-screen transition-colors duration-500"
       style={{ backgroundColor }} // Apply dynamic background color
     >
       <header className="flex items-center justify-center space-x-2 md:justify-start">
@@ -71,9 +37,13 @@ const MainPage = () => {
         showTopTracks={showTopTracks}
         setShowTopTracks={setShowTopTracks}
       />
-
+      <SearchBar />
       {/* Render the SongList component */}
-      <SongList showTopTracks={showTopTracks} />
+      {/* <SongList showTopTracks={showTopTracks} /> */}
+      <SongList
+        showTopTracks={showTopTracks}
+        setSelectedSong={setSelectedSong}
+      />
     </div>
   );
 };
